34190c44aff5614d8adc2eff5929dc2a70b26e73
Timur <tumyp.paxumob@gmail.com>
2022-08-08 21:43:35 +0500

    Added HW

---
+++ Task_54/Program.cs
@@ -0,0 +1,68 @@
+﻿
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+
+Random random = new Random();
+Console.Write("Input quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input quantity of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+int[] arrayRow = new int [rows];
+
+GetArray(array);
+ShowArray(array);
+PrintSortArray(array);
+
+void GetArray(int[,] newArray)
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i,j] = random.Next(1,10);
+        }
+    }
+}
+
+void ShowArray(int[,] newArray)
+{
+     for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+           Console.Write(newArray[i,j] + " ")  ;
+        }
+    Console.WriteLine();
+    }
+}
+
+void PrintSortArray(int[,] newArray)
+{
+    Console.WriteLine("Sorted by rows array: ");
+
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            for(int z = j + 1; z < newArray.GetLength(1); z++)
+            {
+                if(newArray[i,j] > newArray[i,z])
+                {
+                    int temp = newArray[i,j];
+                    newArray[i,j] = newArray[i,z];
+                    newArray[i,z] = temp;
+                } 
+            }  
+            Console.Write(newArray[i,j] + " ");       
+        }   Console.WriteLine();
+    } 
+}
---
+++ Task_58/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 58. Заполнить спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1  2  3  4
+// 12 13 14 5
+// 11 16 15 6
+// 10  9  8  7
+
+
+int rows = 4;
+int columns = 4;
+int[,] array = new int[rows,columns];
+int max = 0;
+
+GetArray(array);
+PrintArray(array);
+
+int MaxElement(int[,] Massive)
+{ 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (Massive[i,j] > max)
+            {
+                max = Massive[i,j];
+            }
+        }
+    }
+    return max;
+}
+
+void GetArray(int[,] Massive)
+{
+    int rows = Massive.GetLength(0);
+    int columns = Massive.GetLength(1);
+
+    for (int j = 0; j < columns; j++)
+    {
+        int i = 0;
+        Massive[i,j] = j + 1;
+        MaxElement(Massive);
+    } 
+ 
+        for (int i = 1; i < rows; i++)
+        {
+            int j = columns - 1;
+            Massive[i,j] = max + 1;
+            MaxElement(Massive);
+        }
+  
+            for (int j = columns - 2; j > 0; j--)
+            {
+                int i = rows - 1;
+                Massive[i,j] = max + 1;
+                MaxElement(Massive);
+            }
+    
+                for (int i = rows - 1; i > 0; i--)
+                {
+                    int j = 0;
+                    Massive[i,j] = max + 1;
+                    MaxElement(Massive);
+                }
+    
+                    for (int j = columns - 3; j < columns - 1; j++)
+                    {
+                        int i = rows - 3;
+                        Massive[i,j] = max + 1;
+                        MaxElement(Massive);
+                    }
+                    
+                        for (int j = columns - 2; j > 0; j--)
+                        {
+                            int i = rows - 2;
+                            Massive[i,j] = max + 1;
+                            MaxElement(Massive);
+                        }
+}
+
+void PrintArray(int[,] ourArray)
+{
+     for (int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < ourArray.GetLength(1); j++)
+        {
+           Console.Write(ourArray[i,j] + "\t")  ;
+        }
+        Console.WriteLine();
+    }
+}
